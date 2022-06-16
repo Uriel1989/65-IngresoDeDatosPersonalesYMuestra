b2b2d45a342b2eb5b71695ec99afb99ffa384316
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:25:51 -0300

    Add files via upload

---
+++ 65-IngresoDeDatosPersonalesYMuestra/HaberQuePasa/DatosDeLasPersonas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HaberQuePasa
+{
+    class DatosDeLasPersonas
+
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int años { get; set; }
+        public string nacionalidad { get; set; }
+
+        public string personas ()
+        {
+            return "Los datos de estas personas son: " + this.nombre + ", apellido " + this.apellido + ", edad " + this.años + ", nacionalidad "+ this.nacionalidad;
+        }
+    }
+}
---
+++ 65-IngresoDeDatosPersonalesYMuestra/HaberQuePasa/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HaberQuePasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 35;
+
+            Console.WriteLine("Vamos a ingresar cantidad de personas y sus datos");
+
+            Console.WriteLine("Ingrese la cantidad de personas:");
+
+            int CantidadDePersonas = Convert.ToInt32(Console.ReadLine());
+
+            List<DatosDeLasPersonas> ListaDePersonas = new List<DatosDeLasPersonas>();
+
+
+            for (int i=0; i< CantidadDePersonas; i++)
+            {
+                DatosDeLasPersonas personas = new DatosDeLasPersonas();
+
+                Console.WriteLine("Ingrese el nombre de la persona " + i +  " :");
+                personas.nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el apellido de la persona "  + i + " :");
+                personas.apellido = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la edad de la persona " + i +" :");
+                personas.años = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese la nacionalidad de la persona " + i+ " :");
+                personas.nacionalidad = Console.ReadLine();
+
+                ListaDePersonas.Add(personas);
+
+                Console.Clear();
+            }
+
+            for ( int i = 0; i< ListaDePersonas.Count; i++)
+            {
+                DatosDeLasPersonas datosDe = ListaDePersonas[i];
+                Console.WriteLine(datosDe.personas());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
